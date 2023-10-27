df620005c6a8b1b30e880760cda3c832868d3657
Ljf227 <ljfrotton@gmail.com>
2023-10-27 13:54:29 -0400

    testing

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,11 +1,19 @@
+import Link from 'next/link'; 
+
 
 export default function Main(){
 
+
     return(
-        <main>
-            <header>
-                
-            </header>
+        <main className="">
+            <header></header>
+
+            <section></section>
+
+            <section></section>
+
+            <footer></footer>
+
         </main>
     ); 
 }
