6f3c05ee476036462d2bbfff021c8a93cb1b7102
Ljf227 <ljfrotton@gmail.com>
2023-10-27 14:14:12 -0400

    form that is basic set up

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -5,10 +5,16 @@ export default function Main(){
 
 
     return(
-        <main className="">
-            <header></header>
+        <main className="grid">
+            <header className="h-40"></header>
 
-            <section></section>
+            <section className ="grid place-items-center border-blue-200 p-3">
+                <form className="flex flex-col gap-2">
+                    <label className="columns-lg text-center text-lg font-bold pb-5">Write me a bit about what you want to start a project with. You don't have to say much!</label>
+                    <textarea />
+                    <button>Submit</button>
+                </form>
+            </section>
 
             <section></section>
 
