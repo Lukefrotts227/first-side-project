c640172125d8e322ca053dbc29d7ac6d6430c841
Ljf227 <ljfrotton@gmail.com>
2023-10-26 20:29:37 -0400

    here it is for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,21 @@
-import { Oswald, Alumni_Sans_Collegiate_One } from "next/font/google"
-const oswald = Oswald({ subsets: ['latin'] })
-const alumni_sans_collegiate_one = Alumni_Sans_Collegiate_One({ subsets: ['latin-ext'], weight: ['400'] })
+import Logo from "@/components/graphics/logo"; 
+import { Permanent_Marker } from "next/font/google"; 
+const perm = Permanent_Marker({ subsets: ['latin'], weight: ['400'] })
 
 export default function Home() {
 
   return (
-    <main className="flex-col items-center justify-between p-24">
+    <main className="grid justify-between p-24">
       <header>
-        <h1 className={`text-center text-7xl pb-14 ${oswald.className}`}>Welcome to my application</h1>
-        <h2 className={`text-center text-4xl ${alumni_sans_collegiate_one.className}`}>I am testing fonts</h2>
       </header>
+
+      <section className="place-items-center"> 
+        <h1 className={`text-center text-7xl ${perm.className}`}>Side Project</h1>
+      </section>
+
+      <section>
+      </section>
+      
 
     </main>
   )
---
+++ components/graphics/logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Logo = () => (
+    <svg width="400" height="300" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg">
+       
+    </svg>
+);
+
+export default Logo;
