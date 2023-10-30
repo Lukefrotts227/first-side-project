8c5ef86aa4ee9c4ceebacba58665f49221fe59ac
Ljf227 <ljfrotton@gmail.com>
2023-10-30 13:15:40 -0400

    testing proper deploy

--- components/main/form.tsx
+++ components/main/form.tsx
@@ -5,15 +5,25 @@ import { Router } from 'next/router';
 const Form = () =>{
     const [content, setContent] = useState(''); 
     const [idea, setIdea] = useState(null);  
+    const [text, setText] = useState(''); 
+    const [isLoading, setIsLoading] = useState(false); 
 
 
     const controller = new AbortController();
     const timeoutId = setTimeout(() => controller.abort(), 10000);
 
+    const handleKeyDown = (e: any) =>{
+        if(e.key === "Enter" && !e.shiftKey){
+            generateIdea(e); 
+            
+        }
+    }
+
 
     const generateIdea = async (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         try {
+            setIsLoading(true); 
             const response = await fetch('/api/main', {
                 method: 'POST',
                 headers: {
@@ -33,17 +43,26 @@ const Form = () =>{
             setIdea(data.choices[0].message.content);
         } catch (error) {
             console.error(error);
-        } 
+        } finally{
+            setIsLoading(false); 
+        }
     }
     
         return(
         <div>
-            <form className="flex flex-col gap-2">
+            <form className="flex flex-col gap-2 items-center justify-center pb-12">
                 <label className="columns-lg text-center text-lg font-bold pb-5">Write me a bit about what you want to start a project with. You don't have to say much!</label>
-                <input value={content}
+                <textarea className="w-full h-11 border-2 border-gray-300 rounded-md text-base" 
+                value={content}
                 onChange={(e) => setContent(e.target.value)}
-                placeholder="Enter it here" />
-                <button onClick={generateIdea}>Submit</button>
+                placeholder="Enter it here"
+                onKeyDown={handleKeyDown}
+                disabled={isLoading} />
+                <button
+                disabled={isLoading} 
+                className="bg-slate-700 w-24 shadow-md border-white text-slate-50 rounded-2xl hover:bg-slate-950 hover:font-extrabold" onClick={generateIdea}>
+                {isLoading ? 'Loading...' : 'Submit'}
+                </button>
             </form>     
             {idea && <div>Generated idea: {idea} </div>}       
         </div>
