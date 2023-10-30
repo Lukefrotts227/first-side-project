26eaff169001e007d336c184ff2917d3b4027da8
Ljf227 <ljfrotton@gmail.com>
2023-10-30 15:34:15 -0400

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'; 
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`min-h-screen bg-gradient-to-bl from-slate-400 to-zinc-100 ${inter.className}`}>{children}
-               
+          <Analytics/>
       </body>
     </html>
   )
