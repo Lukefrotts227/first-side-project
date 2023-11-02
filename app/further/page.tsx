67e0bcbfc7b387bba619cf1238427cff943c0e67
Ljf227 <ljfrotton@gmail.com>
2023-11-02 12:50:57 -0400

    added something to fix the error on npm run build should work

--- app/further/page.tsx
+++ app/further/page.tsx
@@ -1,9 +1,21 @@
 "use client"; 
 import { Display } from "@/components/further/display";
 import { GenerateMap } from "@/components/further/generatemap";
+import { useState, useEffect } from 'react'; 
 
 export default function Further(){
-    if(!sessionStorage.getItem('savedIdeas')){
+    const [canSee, setCanSee]: any = useState(true); 
+    
+    useEffect(() => {
+        let can = sessionStorage.getItem('savedIdeas'); 
+        if(can){
+            setCanSee(true); 
+        }else{
+            setCanSee(false)
+        }
+    }, [])
+
+    if(!canSee){
         return(
             <main>
                 <section className="text-center text-6xl m-12 font-extrabold">You must have an Idea to be here!!!</section>
